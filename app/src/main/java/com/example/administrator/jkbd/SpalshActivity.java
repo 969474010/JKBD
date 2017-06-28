1ca7197f0b9ce8b41ba553dced7abdc14bd0fa4e
ChenYanHui <969474010@qq.com>
2017-06-28 10:19:43 +0800

    添加闪入class

---
+++ app/src/main/java/com/example/administrator/jkbd/SpalshActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.jkbd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+
+public class SpalshActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashactivity);
+        
+
+    }
+
+}
