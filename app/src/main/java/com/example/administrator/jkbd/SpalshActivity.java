fd5dcab01f544de312673fc56ad265486482de18
Administrator <969474010@qq.com>
2017-06-28 15:15:34 +0800

    设置两秒闪屏

--- app/src/main/java/com/example/administrator/jkbd/SpalshActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/SpalshActivity.java
@@ -1,7 +1,9 @@
 package com.example.administrator.jkbd;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 /**
  * Created by Administrator on 2017/6/28.
@@ -12,8 +14,26 @@ public class SpalshActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashactivity);
-        
+
 
     }
+    CountDownTimer timer=new CountDownTimer(3000,1000)
+    {
+        @Override
+        public void onTick(long millisUntilFinished)
+        {
+
+        }
+
+        @Override
+        public void onFinish()
+        {
+            Intent intent=new Intent(SpalshActivity.this,MainActivity.class);
+            startActivity(intent);
+
+        }
+
+
+    };
 
 }
