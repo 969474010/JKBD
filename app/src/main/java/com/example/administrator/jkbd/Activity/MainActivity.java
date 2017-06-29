7709bf89d600a21365568b0443bed4a7cf8c9274
969474010 <969474010@qq.com>
2017-06-29 22:39:00 +0800

    添加点击随机测试到考试页面和退出应用的方法

--- app/src/main/java/com/example/administrator/jkbd/Activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/Activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.administrator.jkbd.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.administrator.jkbd.R;
 
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //进入随机测试
+    public void text(View view) {
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
+    }
+
+    //退出应用
+    public void Exit(View view) {
+        finish();
     }
 }
