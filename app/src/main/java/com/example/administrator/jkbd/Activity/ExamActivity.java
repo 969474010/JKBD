3d8f891e538fc42914ed73ac766cd556ea6ba8e9
Administrator <969474010@qq.com>
2017-06-29 11:12:40 +0800

    创建ExamActivity类

---
+++ app/src/main/java/com/example/administrator/jkbd/Activity/ExamActivity.java
@@ -0,0 +1,22 @@
+package com.example.administrator.jkbd.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.jkbd.R;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class ExamActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_exam);
+
+    }
+}
