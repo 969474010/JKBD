13155c64942d3407dc8fedfbb5c647f924ddd58c
Administrator <969474010@qq.com>
2017-06-30 10:20:13 +0800

    编写完成ExamApplication类

--- app/src/main/java/com/example/administrator/jkbd/Activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/Activity/ExamActivity.java
@@ -5,18 +5,40 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.example.administrator.jkbd.ExamApplication;
 import com.example.administrator.jkbd.R;
+import com.example.administrator.jkbd.bean.ExamInfo;
 
 /**
  * Created by Administrator on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity {
+    TextView tvExamInfo;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_exam);
+        initView();
+        initData();
+    }
 
+    private void initView() {
+        tvExamInfo=(TextView) findViewById(R.id.tv_examinfo);
+    }
+
+    private void initData() {
+        ExamInfo examInfo = ExamApplication.getInstance().getmExamInfo();
+        if(examInfo!=null)
+        {
+            showData(examInfo);
+        }
+
+    }
+
+    private void showData(ExamInfo examInfo) {
+        tvExamInfo.setText(examInfo.toString());
     }
 }
