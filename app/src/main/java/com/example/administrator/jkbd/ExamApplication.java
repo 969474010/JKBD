1b0554668c7964aacc4790d996bda017c7493c7e
Administrator <969474010@qq.com>
2017-06-30 09:57:27 +0800

    给ExamApplication类添加下载网络数据方法

--- app/src/main/java/com/example/administrator/jkbd/ExamApplication.java
+++ app/src/main/java/com/example/administrator/jkbd/ExamApplication.java
@@ -1,7 +1,9 @@
 package com.example.administrator.jkbd;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.example.administrator.jkbd.Utils.OkHttpUtils;
 import com.example.administrator.jkbd.bean.Exam;
 import com.example.administrator.jkbd.bean.ExamInfo;
 
@@ -23,5 +25,24 @@ public class ExamApplication extends Application {
     }
 
     private void initData() {
+        OkHttpUtils<ExamInfo> utils=new OkHttpUtils<>(instance);
+        String uri="http://101.251.196.90:8080/JztkServer/examinfo";
+        utils.url(uri)
+                .targetClass(ExamInfo.class)
+                .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>(){
+
+                    @Override
+                    public void onSuccess(ExamInfo result) {
+                        Log.e("main","result="+result);
+                        mExamInfo=result;
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+
+                    }
+                });
+
     }
 }
